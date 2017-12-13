3a24d32c07b24baefd4ed7ceba55a64bc3e9bbc6
slm1377588711 <1377588711@qq.com>
2017-12-13 15:59:48 +0800

    ss

---
+++ happy/js/liangliang.js
@@ -0,0 +1,8 @@
+/**
+ * Created by slm on 2017-09-28.
+ */
+document.write("<script language=javascript src='js/jquery.cubeSlider.js'></script>");
+document.write("<script language=javascript src='js/javascript.fullPage.js'></script>");
+document.write("<script language=javascript src='js/ImageRotator.js'></script>");
+document.write("<script language=javascript src='js/jquery.waypoints.min.js'></script>");
+document.write("<script language=javascript src='js/jquery.countup.min.js'></script>");
